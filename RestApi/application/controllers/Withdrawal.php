c11a3431d65cd8b482615a5e74aa2c57625a7916
Mikael Kanniainen <kanniainen.mikael@gmail.com>
2020-11-26 13:59:59 +0200

    RestApin päivitys
    Lisätty controller-kansioon account.php, transaction.php ja withdrawal.php lisätty models-kansioon account_model.php, lyhennetty myös url-kyselyä lisäämällä tiedoston juureen .htaccess asetus.

---
+++ RestApi/application/controllers/Withdrawal.php
@@ -0,0 +1,40 @@
+<?php 
+
+defined ('BASEPATH') OR exit('No direct script access allowed');
+
+require APPATH . 'libraries/REST_Controller.php';
+
+/**
+ * This is an example of a RestApi based on PHP and CodeIgniter 3.
+ * 
+ *
+ * @package         CodeIgniter
+ * @subpackage      Rest Server
+ * @category        Controller
+ * @author          Pekka Alaluukas (edited the version made by Phil Sturgeon & Chris Kacerguis)
+ * @license         MIT
+ * @link            https://github.com/chriskacerguis/codeigniter-restserver
+ */
+
+ class Withdrawal extends REST_Controller {
+
+    function __construct()
+    {
+        //enable cors
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        // Construct the parent class
+        parent::__construct();
+
+        $this->load->model('Account_model');
+    }
+
+    public function index_get() {
+
+        $id = $this->uri->segment(2);
+
+        if ($id === NULL) {
+            
+        }
+    }
+ }
