be2c0db27345a8773ff9af0739779cdf7e46f656
Petri Pennanen <t9pepe01@students.oamk.fi>
2020-11-27 18:10:34 +0200

    login

--- RestApi/application/controllers/Login.php
+++ RestApi/application/controllers/Login.php
@@ -1,18 +1,14 @@
 <?php
-
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-require APPPATH . 'libraries/REST_Controller.php';
-
-
 class Login extends CI_Controller {
-    public function check_login(){
-        $this->load->model('User_model');
-        $username=$this->input->get('idkortti');
-        $plaintext_password=$this->input->get('tunnusluku');
-        $encrypted_password=$this->User_model->check_login($username);
+    public function index(){
+        $this->load->model('Login_model');
+        $idcard=$this->input->get('idkortti');
+        $plaintext_pinCode=$this->input->get('tunnusluku');
+        $encrypted_pinCode=$this->Login_model->check_login($idcard);
 
-        if(password_verify($plaintext_password,$encrypted_password)){
+        if(password_verify($plaintext_pinCode, $encrypted_pinCode)){
           $result=true;
         }
         else{
